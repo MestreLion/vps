cc9fe8985ac22728642f09011c92d2f54e99c35f
MestreLion <linux@rodrigosilva.com>
2020-08-18 00:30:30 -0300

    improve bootsrap documentation

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,14 @@
 # Copyright (C) 2020 Rodrigo Silva (MestreLion) <linux@rodrigosilva.com>
 # License: GPLv3 or later, at your choice. See <http://www.gnu.org/licenses/gpl>
 #
+# Tasks performed:
+# - Install git from distribution repositories
+# - Clone project git repository, by default from Github
+# - Update (pull) the local git repository
+#
 # Instructions:
 # - Choose "Ubuntu 20.04" image (/etc/ssh/sshd_config.d/ does not exist in 18.04)
-# - Set root password (root SSH login will be disabled by this setup)
+# - Set root password (root SSH login will be disabled by default)
 # - Log in via Cloud Provider VPS Web Console
 # - Copy in terminal:
 #
@@ -15,19 +20,24 @@
 #
 ###############################################################################
 
-set -Eeuo pipefail  # exits on any errors
+set -Eeuo pipefail  # exit on any error
 trap '>&2 echo "error in line $LINENO, code $?, command: $BASH_COMMAND"' ERR
 
 export VPS_CONFIG=${1:-${VPS_CONFIG:-'/etc/vps/vps.conf'}}
-
-if [[ -r "$VPS_CONFIG" ]]; then
-	source "$VPS_CONFIG"
-fi
+if [[ -r "$VPS_CONFIG" ]]; then source "$VPS_CONFIG"; fi
 
 ###############################################################################
 
-VPS_REPO=${VPS_REPO:-'https://github.com/MestreLion/vps.git'}
-VPS_DIR=${VPS_DIR:-'/opt/vps'}
+export VPS_REPO=${VPS_REPO:-'https://github.com/MestreLion/vps.git'}
+export VPS_DIR=${VPS_DIR:-'/opt/vps'}
+
+for arg in "$@"; do if [[ "$arg" == '-h' || "$arg" == '--help' ]]; then
+	echo "Bootstrap VPS initial setup"
+	echo "Install git, clone and update local repository"
+	echo "See $VPS_REPO for details"
+	echo "Usage: bootstrap.sh [CONFIG_FILE]"
+	exit
+fi; done
 
 if [[ "$(id -u)" -ne 0 ]]; then
 	echo "You must run this as root" >&2
